c87a4344569a571681e1c45fbffc44da2e4fb875
Mike Khaw <mikekhawnow@gmail.com>
2024-07-11 21:57:53 -0700

    saving work in app.py. getting error Object of type Document is not JSON serializable when testing locally. stopping here for today

--- app.py
+++ app.py
@@ -35,8 +35,9 @@ from_cloud_qdrant = Qdrant.from_existing_collection(
     api_key=QDRANT_CLOUD_KEY
 )
 
-# And then convert it to a retriever so it can be used in our chain
+# Convert retrieved documents to JSON-serializable format
 base_retriever = from_cloud_qdrant.as_retriever()
+
 
 # -- AUGMENTED -- #
 
@@ -51,7 +52,12 @@ base_llm = ChatOpenAI(
 base_rag_prompt_template = """\
 You are a friendly AI assistant. Using the provided context, please answer the user's question in a friendly, conversational tone. 
 
-Provide the top 3 options if available. For each option, provide a brief description along with the respective URL. If you don't know the answer based on the context, say you don't know. 
+Provide the top 3 options if available. For each option, provide the following information:
+1. A brief description of the recipe
+2. The URL of the recipe
+3. The ratings and number of ratings
+
+If you don't know the answer based on the context, say you don't know. 
 
 After providing your answer, always prompt the user for feedback or more questions in order to continue the conversation.
 
@@ -67,7 +73,7 @@ base_rag_prompt = ChatPromptTemplate.from_template(base_rag_prompt_template)
 
 # -- GENERATION -- #
 
-# A function that will be executed at the start of a user session
+# Chat Start Function: Initialize a RAG (Retrieval-Augmented Generation) chain at the start of each chat session.
 @cl.on_chat_start
 async def start_chat():
     """
@@ -76,25 +82,33 @@ async def start_chat():
     The user session is a dictionary that is unique to each user session, and is stored in the memory of the server.
     """ 
     base_rag_chain = (
+        # INVOKE CHAIN WITH: {"question" : "<<SOME USER QUESTION>>"}
+        # "question" : populated by getting the value of the "question" key
+        # "context"  : populated by getting the value of the "question" key and chaining it into the base_retriever
         {"context": itemgetter("question") | base_retriever, "question": itemgetter("question")}
+        # "context"  : is assigned to a RunnablePassthrough object (will not be called or considered in the next step)
+        #              by getting the value of the "context" key from the previous step
         | RunnablePassthrough.assign(context=itemgetter("context"))
+        # "response" : the "context" and "question" values are used to format our prompt object and then piped
+        #              into the LLM and stored in a key called "response"
+        # "context"  : populated by getting the value of the "context" key from the previous step
         | {"response": base_rag_prompt | base_llm | StrOutputParser(), "context": itemgetter("context")}
     )
     cl.user_session.set("base_rag_chain", base_rag_chain)
     
     
- # A function that should be run each time the chatbot receives a message from a user
+ # Message Handling Function: Process and respond to user messages using the RAG chain.
 @cl.on_message  
 async def main(message: cl.Message):
     """
     This function will be called every time a message is recieved from a session.
-    We will use the LCEL RAG chain to generate a response to the user query.
+    We will use the LCEL RAG chain to generate a response to the user question.
     The LCEL RAG chain is stored in the user session, and is unique to each user session - this is why we can access it here.
     """
     base_rag_chain = cl.user_session.get("base_rag_chain")
     msg = cl.Message(content="")
     async for chunk in base_rag_chain.astream(
-        {"query": message.content},
+        {"question": message.content},
         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
     ):
         await msg.stream_token(chunk)
